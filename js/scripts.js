f4eaecc8c327af136d0bf483957659cfaf8fd9f4
Heather <Heather>
2015-02-18 00:03:20 +0000

    Add specific address lines to form.

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,36 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var inputtedAddress = $("input#new-address").val();
-    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress };
+    var inputtedStreet = $("input#new-street").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
+    var inputtedZip = $("input#new-zip").val();
+    var newAddress = { street: inputtedStreet,
+                        city: inputtedCity,
+                        state: inputtedState,
+                        zipcode: inputtedZip,
+                        fullAddress: function() {
+                          return this.street + ", " + this.city + ", " + this.state + ", " + this.zipcode;
+                        }
+                      };
+    var newContact = { firstName: inputtedFirstName,
+                        lastName: inputtedLastName,
+                        address: newAddress.fullAddress()
+                        };
+
+
+// ----------------------------------------------------------------
+
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
-    $("input#new-address").val("");
+    $("input#new-street").val("");
+    $("input#new-city").val("");
+    $("input#new-state").val("");
+    $("input#new-zip").val("");
+
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
